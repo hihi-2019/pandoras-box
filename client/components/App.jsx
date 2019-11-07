6604c083915ad26468f4965ac1a09e1c1a2fee8d
Student <student@eda-bc-07.localdomain>
2019-11-07 17:56:28 +1300

    add links from buttons

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route, Link} from 'react-router-dom'
 import AmbulanceSim from './AmbulanceSim'
 import CarAlarm from './CarAlarm'
 import Seizure from './Seizure'
@@ -12,10 +12,10 @@ const App = () => {
       <h1>Pandora's Box</h1>
       </div>
       <div>
-        <button>Ambulance</button>
-        <button>Car Alarm</button>
-        <button>Seizure</button>
-        <button>Spiders</button>
+        <Link to ="/ambulance"><button>Ambulance</button></Link>
+        <Link to ="/car"><button>Car Alarm</button></Link>
+        <Link to ="/seizure"><button>Seizure</button></Link>
+        <Link to ="/spiders"><button>Spiders</button></Link>
       </div>
       <div class="container">
         <Route exact path="/ambulance" component={AmbulanceSim}/>
